2fa974ba50387569854b7e4d2318638d6c3f1a0d
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-04 00:57:29 -0600

    Update Simulator.java

--- Intermediate_Java_5_Projects/1_BuildADiceRollingSimulator/Simulator.java
+++ Intermediate_Java_5_Projects/1_BuildADiceRollingSimulator/Simulator.java
@@ -3,12 +3,32 @@ package com.isai.dice;
 //import to generate random numbers
 import java.util.Random;
 
+//import scanner to take values from user
+import java.util.Scanner;
+
+//import missmatch to handle non int values
+import java.util.InputMismatchException;
+
 public class Simulator {
     public static void main(String[] args) {
-        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        int rolledNumber = rand.nextInt(6) + 1;
-        System.out.println(display(rolledNumber));
+        try{
+            System.out.println("How many dice would you like to roll?");
+            int numberOfDice = scanner.nextInt();
+
+            System.out.println("About to roll " + numberOfDice + " dice");
+
+            Random rand = new Random();
+
+            for (int i = 0; i < numberOfDice; i++) {
+                int rolledNumber = rand.nextInt(6) + 1;
+                System.out.println(display(rolledNumber));
+            }
+        }catch (InputMismatchException e){
+            System.out.println("That is not a valid number");
+        }
+
     }
 
     /*
@@ -18,21 +38,14 @@ public class Simulator {
     * the random number thrown
     * */
     static String display(int value){
-        switch (value){
-            case 1:
-                return "---------\n|       |\n|   o   |\n|       |\n---------";
-            case 2:
-                return "---------\n| o     |\n|       |\n|     o |\n---------";
-            case 3:
-                return "---------\n| o     |\n|   o   |\n|     o |\n---------";
-            case 4:
-                return "---------\n| o   o |\n|       |\n| o   o |\n---------";
-            case 5:
-                return "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
-            case 6:
-                return "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
-            default:
-                return "Not a valid dice number";
-        }
+        return switch (value){
+            case 1 -> "---------\n|       |\n|   o   |\n|       |\n---------";
+            case 2 -> "---------\n| o     |\n|       |\n|     o |\n---------";
+            case 3 -> "---------\n| o     |\n|   o   |\n|     o |\n---------";
+            case 4 -> "---------\n| o   o |\n|       |\n| o   o |\n---------";
+            case 5 -> "---------\n| o   o |\n|   o   |\n| o   o |\n---------";
+            case 6 -> "---------\n| o   o |\n| o   o |\n| o   o |\n---------";
+            default -> "Not a valid dice number";
+        };
     }
 }
