1946560183b4519bcd0ad472f05faa36c36fac3e
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-06 00:12:59 -0600

    Create app_f.py

---
+++ Domina_Python_Manipulación_de_datos_con_Pandas/01_08_AgregarColumnaADataFrameEnPandas/app_f.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df_clientes = pd.DataFrame({
+  "nombre" : ["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella"],
+  "edad" : [35, 30, 32, 41, 47, 23, 20, 28], 
+  "cuidad" : ["Buenos Aires", "Lima", "Ciudad de México", "Santiago", "Alajuela", "Nueva York", "Toronto", "Río de Janeiro"]
+})
+
+lista_profesion = ["Médico", "Ingeniera civil", "Abogado", "Profesora", "Contador", "Enfermera", "Estudiante", "Periodista"]
+df_clientes['profesion'] = lista_profesion
+
+print(df_clientes)
