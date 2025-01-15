0b5293903de866ae3fe7679d972d307bdee10026
Isai <47548422+isaiisj@users.noreply.github.com>
2025-01-15 17:48:55 -0600

    Update app_f.py

--- Domina_Python_Manipulación_de_datos_con_Pandas/02_13_EliminarColumnasYFilasEnDataFrameEnPandas/app_f.py
+++ Domina_Python_Manipulación_de_datos_con_Pandas/02_13_EliminarColumnasYFilasEnDataFrameEnPandas/app_f.py
@@ -6,6 +6,10 @@ df_empleados = pd.read_csv(archivo, delimiter=";")
 print("Dataframe original:")
 print(df_empleados)
 
-df_empleados = df_empleados[df_empleados['Experiencia'] >= 5]
-print("Dataframe con empleados de 5 o más años de experiencia:")
+df_empleados = df_empleados.drop(columns=['Estado_Civil'])
+print("Dataframe sin estado civil:")
+print(df_empleados)
+
+df_empleados = df_empleados.drop(df_empleados.index[0])
+print("Dataframe después de eliminar el primer registro:")
 print(df_empleados)
